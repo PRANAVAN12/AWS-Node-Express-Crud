cb037025579aec5bc5ce54b749b48d2aa4668f1d
Pranavan <npranavan28@gmail.com>
2022-04-08 12:47:34 +0530

    Folder structured changes and Delete and Update api integrated

---
+++ src/routes/users/index.js
@@ -0,0 +1,133 @@
+const AWS = require("aws-sdk");
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+app.use(express.json());
+
+app.get("/users/:userId", async function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  try {
+    const { Item } = await dynamoDbClient.get(params).promise();
+    if (Item) {
+      const { userId, name ,age , mobile} = Item;
+      res.json({ userId, name ,age, mobile});
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive user" });
+  }
+});
+
+
+
+app.delete("/users/:userId", async function (req, res) {
+  
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  try {
+    const deletedUser = await dynamoDbClient.delete(params).promise();
+    if (deletedUser) {
+      res.json({ success: 'Sucessfully deleted this user' });
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive user" });
+  }
+});
+
+app.post("/users", async function (req, res) {
+  const { userId, name ,age, mobile } = req.body;
+  if (typeof userId !== "string") {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== "string") {
+    res.status(400).json({ error: '"name" must be a string' });
+  } else if (typeof age == "string") {
+    res.status(400).json({ error: '"age" must be a number' });
+  } else if (typeof mobile !== "string") {
+    res.status(400).json({ error: '"mobile" must be a string' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId: userId,
+      name: name,
+      age: age,
+      mobile: mobile,
+    },
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    res.json({ userId, name, age, mobile });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create user" });
+  }
+});
+app.patch("/users/:userId", async function (req, res) {
+  const { userId, name ,age, mobile } = req.body;
+  if (typeof userId !== "string") {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== "string") {
+    res.status(400).json({ error: '"name" must be a string' });
+  } else if (typeof age == "string") {
+    res.status(400).json({ error: '"age" must be a number' });
+  } else if (typeof mobile !== "string") {
+    res.status(400).json({ error: '"mobile" must be a string' });
+  }
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId: userId,
+      name: name,
+      age: age,
+      mobile: mobile,
+    },
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    res.json({ userId, name, age, mobile });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create user" });
+  }
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+
+module.exports.handler = serverless(app);
